2e214220364f237c3fa14865b424601b6161bd60
bonolo baker <bakerbnl@gmail.com>
2015-04-08 20:14:53 +0200

    TestApp
    test pages.

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,23 @@
+class StaticPagesController < ApplicationController
+	def home 
+	end
+
+	def cvwriting
+	end
+
+	def interviews
+	end
+
+	def work
+	end
+
+	def feedback
+	end
+
+	def disclaimer
+	end
+
+	def sitemap
+	end
+	
+end
